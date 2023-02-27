b51fde62b79a2c524d151881ef50f73326f0f68b
Rew <andriyvak1233@gmail.com>
2023-02-27 22:49:49 +0200

    form

--- event.js
+++ event.js
@@ -35,45 +35,87 @@
 
 /*-----------------------ПАТТЕРН ОБ'ЄКТ ПОСИЛАНЬ-------------------------------------------*/
 
-const refs = {
-    input : document.querySelector('.js-input'),
-    nameLabel : document.querySelector('.js-button > span'),
-    licenseCheckbox : document.querySelector('.js-license'),
-    btn : document.querySelector('.js-button'),
-}
+// const refs = {
+//     input : document.querySelector('.js-input'),
+//     nameLabel : document.querySelector('.js-button > span'),
+//     licenseCheckbox : document.querySelector('.js-license'),
+//     btn : document.querySelector('.js-button'),
+// }
 
 // const input = document.querySelector('.js-input');
 // const nameLabel = document.querySelector('.js-button > span');
 // const license = document.querySelector('.js-license');
 // const btn = document.querySelector('.js-button');
 
-function onInputFocus() {
-    console.log('Імпут отримав фокус - подія focus');
-}
+// function onInputFocus() {
+//     console.log('Імпут отримав фокус - подія focus');
+// }
     
-function onInputBlur() {
-    console.log('Імпут втратив фокус - подія blur');
-}
+// function onInputBlur() {
+//     console.log('Імпут втратив фокус - подія blur');
+// }
 
-refs.input.addEventListener('focus', onInputFocus);
-refs.input.addEventListener('blur', onInputBlur);
+// refs.input.addEventListener('focus', onInputFocus);
+// refs.input.addEventListener('blur', onInputBlur);
 
-refs.input.addEventListener('change', onInputChange);
-refs.input.addEventListener('input', onInputChangeAll);
+// refs.input.addEventListener('change', onInputChange);
+// refs.input.addEventListener('input', onInputChangeAll);
 
-function onInputChange(event) { 
-    console.log(event.currentTarget.value);
-};
+// function onInputChange(event) { 
+//     console.log(event.currentTarget.value);
+// };
 
-function onInputChangeAll(event) {
-    console.log(event.currentTarget.value);
-    refs.nameLabel.textContent = event.currentTarget.value;
-}
+// function onInputChangeAll(event) {
+//     console.log(event.currentTarget.value);
+//     refs.nameLabel.textContent = event.currentTarget.value;
+//     console.log(refs.nameLabel.textContent.length)
+// }
 
-refs.licenseCheckbox.addEventListener('change', onLicenseChange);
+// refs.licenseCheckbox.addEventListener('change', onLicenseChange);
 
-function onLicenseChange(event) {
-    console.log(event.currentTarget.checked)
+// function onLicenseChange(event) {
+//     console.log(event.currentTarget.checked)
 
-    refs.btn.disabled = !event.currentTarget.checked;
-}
+//     refs.btn.disabled = !event.currentTarget.checked;
+// }
+
+
+// const button = document.querySelector(".button");
+
+// const handleClick = (event) => {
+//   console.log("event: ", event);
+//   console.log("event type: ", event.type);
+//   console.log("currentTarget: ", event.currentTarget);
+// };
+
+// button.addEventListener("click", handleClick);
+
+
+// const imgEl = document.querySelector('.js-img');
+// const swapBtnEl = document.querySelector('.js-swap-btn');
+// const removeListenerBtnEl = document.querySelector('.js-remove-listener');
+
+// const handleSwapImg = () =>{
+//     imgEl.src = 'https://picsum.photos/200/300';
+// }
+
+// swapBtnEl.addEventListener('click', handleSwapImg);
+// removeListenerBtnEl.addEventListener('click', () => {
+//     swapBtnEl.removeEventListener('click', handleSwapImg);
+// }
+
+const formEl = document.querySelector('.js-contact-form');
+const formInputEl = document.querySelector('.js-username-input');
+const formCheckboxEl = document.querySelector('.js-policy-checkbox');
+const userNameOutputEl = document.querySelector('.js-username-output');
+const formSubmitBtnEl = document.querySelector('.js-contact-form-submit');
+
+formInputEl.addEventListener('focus', (event) => {
+    console.log(event);
+    event.target.style.outline = '4px solid teal';
+})
+
+formInputEl.addEventListener('blur', (event) => {
+    event.target.style.outline = 'none'
+})
+
